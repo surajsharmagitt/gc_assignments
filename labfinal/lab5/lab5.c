d27cabd92382be48d867e49014363b590be78581
Suraj Sharma <sachinsharma91829@gmail.com>
2026-04-02 14:14:19 +0530

    basic tree codes

---
+++ labfinal/lab5/lab5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure of a node
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+// Function to create a new node
+struct Node *createNode(int value)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// Inorder Traversal (Left → Root → Right)
+void inorder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    // Creating tree manually
+    /*
+            1
+           / \
+          2   3
+         / \
+        4   5
+    */
+
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+
+    return 0;
+}
